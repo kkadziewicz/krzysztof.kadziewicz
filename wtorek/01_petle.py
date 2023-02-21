c4609a8987028dba5d0a5fe2ff39edebc3c08053
Krzysztof <krzysztof@kadziewicz.eu>
2023-02-21 11:16:45 +0100

    pętla

--- wtorek/01_petle.py
+++ wtorek/01_petle.py
@@ -1,2 +1,13 @@
 # petla interakcyjna + iterables
 
+imie = "Krzysztof"
+wiek = 55
+lista_2 = [3.14, 6, 12]
+lista = [3, 54, 3, wiek,  5, 8, lista_2,  imie, 9]
+
+for element in lista:
+    print(f"Wartość {element} ma typ {type(element)}")
+    if isinstance(element, int):
+        print(f"Litera to {chr(element)}")
+    else:
+        print("Sorry")

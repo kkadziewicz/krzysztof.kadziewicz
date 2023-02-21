41aa3fbf7331f2546d26a2a5f5d50cc62ebfb37e
Krzysztof <krzysztof@kadziewicz.eu>
2023-02-21 11:37:26 +0100

    pętla

--- wtorek/04_petle.py
+++ wtorek/04_petle.py
@@ -9,5 +9,9 @@ for element in lista:
     print(f"Wartość {element} ma typ {type(element)}")
     if isinstance(element, int):
         print(f"Litera to {chr(element)}")
+    elif isinstance(element, list):
+        print(f"Teraz lista wewnętrzna - {element}")
+        for inny in element:
+            print(f"Teraz lista wewnętrzna - {chr(inny)}")
     else:
         print("Sorry")

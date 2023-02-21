f3ad6a4cf04ec59417a399ade4a61982f8442384
Krzysztof <krzysztof@kadziewicz.eu>
2023-02-21 10:41:03 +0100

    składnia

--- wtorek/petle.py
+++ wtorek/petle.py
@@ -8,3 +8,8 @@ print("---------------------------------") #nie robić na produkcji
 
 for value in range(1, 5):
     print(f"Zmienna sterująca = {value=}")
+
+print("---------------------------------") #nie robić na produkcji
+
+for value in range(1, 15, 3):
+    print(f"Zmienna sterująca = {value=}")

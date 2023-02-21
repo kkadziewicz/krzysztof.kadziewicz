6bb11723fa5f5ebc652fd8022585672dcc918f45
Krzysztof <krzysztof@kadziewicz.eu>
2023-02-21 10:39:05 +0100

    składnia

---
+++ wtorek/petle.py
@@ -0,0 +1,10 @@
+# petla interakcyjna
+
+for value in range(5):
+    # print(f"Zmienna sterująca = {value}")
+    print(f"Zmienna sterująca = {value=}")
+
+print("---------------------------------") #nie robić na produkcji
+
+for value in range(1, 5):
+    print(f"Zmienna sterująca = {value=}")

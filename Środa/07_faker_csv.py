ccaa4cc5c4cd856570359f94a90df4e719c0f6d8
Krzysztof <krzysztof@kadziewicz.eu>
2023-02-22 13:44:45 +0100

    moduly

--- Środa/07_faker_csv.py
+++ Środa/07_faker_csv.py
@@ -4,6 +4,6 @@ from faker import Faker
 ludzik = Faker("pl-PL")
 
 with open("ludzie.csv", "w", newline='') as plik_csv:
-    csv_writer = csv.writer(plik_csv, delimiter=" ", quotechar="|", quoting=csv.QUOTE_MINIMAL)
+    csv_writer = csv.writer(plik_csv, quoting=csv.QUOTE_NONNUMERIC) #, delimiter=" ", quotechar="|"
     for _ in range(10):
         csv_writer.writerow([ludzik.name(), ludzik.company(), ludzik.company_vat(), ludzik.local_regon()])

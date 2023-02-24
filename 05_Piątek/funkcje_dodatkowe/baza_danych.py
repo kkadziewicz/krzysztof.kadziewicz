f0d512dac10a2fd97e3fb61054eb22dc3b48dd1d
Krzysztof <krzysztof@kadziewicz.eu>
2023-02-24 11:19:26 +0100

    piątek

--- 05_Piątek/funkcje_dodatkowe/baza_danych.py
+++ 05_Piątek/funkcje_dodatkowe/baza_danych.py
@@ -1,6 +1,13 @@
 from secrets import token_hex
+from sqlite3 import *
+print(f"blah blah blah - {__name__} / To jest przetwarzane przy jakimkolwiek imporcie")
+
+# https://sqlitebrowser.org/
 
 def generate_token():
     new_token = token_hex(60)
     print(new_token)
     return new_token
+
+def create_user_record(user, token):
+    print(user, token)

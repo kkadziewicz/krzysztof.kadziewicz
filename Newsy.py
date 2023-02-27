a937c11fe57c9e85322b570aea3c48cec0bbf25f
Krzysztof Kadziewicz <krzysztof@kadziewicz.eu>
2023-02-27 12:48:46 +0100

    Newsy

--- Newsy.py
+++ Newsy.py
@@ -2,45 +2,43 @@ import requests
 import tkinter as tk
 import webbrowser
 
-# Zdefiniuj klucz API do NewsAPI.org
-api_key = "3d797c82c9b04851b1d74bf763bf5a7e"
+# Zdefiniuj klucz API do newsapi
+API_KEY = '3d797c82c9b04851b1d74bf763bf5a7e'
 
-# Definiuj URL do API z parametrem języka polskiego
-url = f"https://newsapi.org/v2/top-headlines?country=pl&language=pl&apiKey={api_key}"
-
-# Funkcja, która pobiera i wyświetla newsy po naciśnięciu przycisku
-def get_news():
-    # Wywołaj API i zapisz wynik w zmiennej response
-    response = requests.get(url)
-
-    # Sprawdź, czy zapytanie zakończyło się powodzeniem
-    if response.status_code == 200:
-        # Wyczyść poprzednie wyniki
-        news_list.delete(0, tk.END)
-
-        # Wyświetl tytuł i link każdego artykułu
-        for article in response.json()['articles']:
-            news_title = article['title']
-            news_url = article['url']
-            news_list.insert(tk.END, news_title)
-            # Dodaj link do tagu tekstowego dla każdego newsa
-            news_list.tag_add(news_url, f"{news_list.size()-1}.0", f"{news_list.size()-1}.end")
-            # Dodaj zdarzenie kliknięcia do linku
-            news_list.tag_bind(news_url, "<Button-1>", lambda event, url=article['url']: webbrowser.open_new(url))
-    else:
-        news_list.insert(tk.END, "Wystąpił problem z pobieraniem newsów.")
-
-# Stwórz okno główne
+# Utwórz okno programu
 root = tk.Tk()
-root.title("Aktualności")
+root.title('Najnowsze newsy')
 
-# Stwórz przycisk do pobierania newsów
-get_news_button = tk.Button(root, text="Pobierz newsy", command=get_news)
-get_news_button.pack(pady=10)
+# Utwórz etykietę z tytułem programu
+title_label = tk.Label(root, text='Najnowsze newsy', font=('Arial', 18))
+title_label.pack(pady=10)
 
-# Stwórz listę do wyświetlania newsów
-news_list = tk.Text(root, width=80, height=30)
-news_list.pack()
+# Utwórz pole tekstowe do wyświetlania newsów
+news_text = tk.Text(root, width=80, height=20, font=('Arial', 12))
+news_text.pack(padx=10, pady=10)
 
-# Uruchom pętlę zdarzeń interfejsu graficznego
+# Wykonaj zapytanie do API, aby pobrać najnowsze newsy
+response = requests.get('https://newsapi.org/v2/top-headlines?country=pl&apiKey=' + API_KEY)
+
+# Sprawdź, czy żądanie zakończyło się sukcesem
+if response.status_code == 200:
+    # Przetwórz odpowiedź z serwera i wyświetl tytuły newsów
+    news = response.json()
+    for article in news['articles']:
+        title = article['title']
+        url = article['url']
+        news_text.insert(tk.END, title + '\n')
+        news_text.insert(tk.END, url + '\n\n')
+        news_text.tag_add('link', f"{news_text.index(tk.INSERT)}-{len(url)}c", news_text.index(tk.INSERT))
+        news_text.tag_config('link', foreground='blue', underline=True)
+        news_text.tag_bind('link', '<Button-1>', lambda event, url=url: webbrowser.open_new(url))
+else:
+    # Wyświetl komunikat o błędzie
+    news_text.insert(tk.END, 'Błąd pobierania newsów\n\n')
+
+# Utwórz przycisk do zamykania programu
+close_button = tk.Button(root, text='Zamknij', command=root.destroy)
+close_button.pack(pady=10)
+
+# Uruchom pętlę główną programu
 root.mainloop()

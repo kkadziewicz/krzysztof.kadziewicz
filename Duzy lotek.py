6c0b7b91799f661b65656457cc3b7afd0a52293d
Krzysztof Kadziewicz <krzysztof@kadziewicz.eu>
2023-03-02 11:50:12 +0100

    lotek

--- Duzy lotek.py
+++ Duzy lotek.py
@@ -1,22 +1,16 @@
-# Importujemy modu³ random do losowania liczb
 import random
 
-# Definiujemy funkcjê, która generuje szeœæ liczb od 1 do 49
-def lotto():
-    # Tworzymy pust¹ listê na wylosowane liczby
-    liczby = []
-    # Powtarzamy szeœæ razy
-    for i in range(6):
-        # Losujemy liczbê od 1 do 49
-        liczba = random.randint(1, 49)
-        # Sprawdzamy, czy liczba nie jest ju¿ na liœcie
-        while liczba in liczby:
-            # Jeœli tak, to losujemy inn¹ liczbê
-            liczba = random.randint(1, 49)
-        # Dodajemy liczbê do listy
-        liczby.append(liczba)
-    # Zwracamy listê wylosowanych liczb
-    return liczby
+# Tworzenie pustej listy, do ktÃ³rej bÄ™dÄ… dodawane wylosowane liczby
+lotto_numbers = []
 
-# Wywo³ujemy funkcjê i wyœwietlamy wynik
-print(lotto())
+# PÄ™tla while losujÄ…ca 6 unikalnych liczb z zakresu od 1 do 49
+while len(lotto_numbers) < 6:
+    number = random.randint(1, 49)
+    if number not in lotto_numbers:
+        lotto_numbers.append(number)
+
+# Sortowanie wylosowanych liczb od najmniejszej do najwiÄ™kszej
+lotto_numbers.sort()
+
+# WyÅ›wietlenie wylosowanych liczb
+print("Wylosowane liczby to:", lotto_numbers)
